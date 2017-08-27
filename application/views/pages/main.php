8fc8250e8adc687ced854e186d766b09a9832c9e
Brad <mrwilson1@comcast.net>
2017-08-27 09:14:06 -0400

    Making the main page layout

---
+++ application/views/pages/main.php
@@ -0,0 +1,24 @@
+<body>
+	<div class="l-containers-main">
+		<div class="l-headers-main">
+			<div class="l-headers-blog--title">
+				<h1>Welcome</h1>
+			</div>
+		</div>
+		<div class="l-containers-main--left">
+			<div class="l-containers-main--left--shell">
+				
+			</div>			
+		</div>
+		<div class="l-containers-main--middle">
+			<div class="l-containers-main--middle--shell">
+				
+			</div>			
+		</div>
+		<div class="l-footers-main">
+			<div class="l-footers-main--shell">
+				
+			</div>
+		</div>
+	</div>
+</body>
