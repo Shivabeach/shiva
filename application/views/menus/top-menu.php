ca9daa1526525cb6f2e7c98d379fdc546c9bb277
Brad <mrwilson1@comcast.net>
2018-02-01 18:28:45 -0500

    changing colors

--- application/views/menus/top-menu.php
+++ application/views/menus/top-menu.php
@@ -3,9 +3,9 @@
         <div class="l-menu">
             <ul class="menu-flex">
                 <li class="hova home"><?php echo anchor('pages', 'Home', array('class' => 'icon fa-home'));?></li>
-                <li class="hova"><?php echo anchor('blogs', 'Blog');?></li>
-                <li class="hova"><?php echo anchor('grid', 'Grid');?></li>
-                <li class="hova"><?php echo anchor('grid/newgrid', 'New Grid');?></li>
+                <li class="hova"><?php echo anchor('blogs', 'Blog', array("rel" => "external"));?></li>
+                <li class="hova"><?php echo anchor('grid', 'Grid', array("rel" => "external"));?></li>
+                <li class="hova"><?php echo anchor('grid/newgrid', 'New Grid', array("rel" => "external"));?></li>
                 <li class="hova"><a href="http://bare/" rel="external">Bare Grid</a></li>
                 <li class="hova"><a href="http://trial/pages/gridd" rel="external">Trial Griddd</a></li>
                 <li class="hova"><?php echo anchor("http://grid/", "Griddd System", array("rel" => "external"));?></li>
