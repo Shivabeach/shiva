d6cb2a74703c661a24fd848c78c393e644dc4fb6
Brad <mrwilson1@comcast.net>
2018-02-13 08:06:54 -0500

    updating pages removing pages

--- application/views/menus/top-menu.php
+++ application/views/menus/top-menu.php
@@ -5,7 +5,6 @@
                 <li class="hova home"><?php echo anchor('pages', 'Home', array('class' => 'icon fa-home'));?></li>
                 <li class="hova"><?php echo anchor('blogs', 'Blog', array("rel" => "external"));?></li>
                 <li class="hova"><?php echo anchor('grid', 'Grid', array("rel" => "external"));?></li>
-                <li class="hova"><?php echo anchor('grid/newgrid', 'New Grid', array("rel" => "external"));?></li>
                 <li class="hova"><a href="http://bare/" rel="external">Bare Grid</a></li>
                 <li class="hova"><a href="http://trial/pages/gridd" rel="external">Trial Griddd</a></li>
                 <li class="hova"><?php echo anchor("http://grid/", "Griddd System", array("rel" => "external"));?></li>
